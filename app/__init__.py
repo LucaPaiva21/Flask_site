f714d43b0d14f35c57ff7b5ee90935fbce059260
LucaPaiva <leandrolluca@hotmail.com>
2023-08-31 16:24:42 -0300

    tua mae é minha boe

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'synceduc'
+
 from app import routes
 
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, flash, redirect
+from app.forms import Cadastro
+
 
 @app.route("/")
 def index():
@@ -15,9 +17,28 @@ def sobre():
 
 @app.route("/cadastro")
 def cadastro():
-    return render_template('cadastro.html', titulo = "cadastro")
+    cadastro = Cadastro()
+    nome = cadastro.nome.data
+    email = cadastro.email.data
+    nacimento = cadastro.nascimento.data
+    senha = cadastro.senha.data
+    if cadastro.validate_on_submit():
+        flash("Dados enviados com sucesso")
+        redirect('cadastro.html')
+    return render_template('cadastro.html', titulo = "cadastro", cadastro = cadastro)
 
 @app.route("/login")
 def login():
     return render_template('login.html', titulo = "login")
 
+
+@app.route("/dados", methods=['GET', 'POST'])
+def dados(): 
+    nome = request.args.get('nome')
+    email = request.args.get('email')
+    nascimento = request.args.get('data-nascimento')
+    senha = request.args.get('senha')
+    if len(senha) != 0:
+        flash("Campo senha vazio")
+        redirect('cadastro.html')
+    return f"Nome: {nome}\nEmail: {email}\nNascimento: {nascimento}\nSenha: {senha}"
